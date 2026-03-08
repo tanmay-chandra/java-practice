d1e8a7117fa34bab40e7d40ce3fa050b37592284
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-08 12:20:08 +0530

    Changed folder name

---
+++ ArithematicOperator.java
@@ -0,0 +1,13 @@
+public class ArithematicOperator {
+    public static void main(String[]args){
+        int a=10,b=5;
+
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Divison:"+(a/b));
+        System.out.println("Modulus:"+(a%b));
+
+    }
+    
+}
