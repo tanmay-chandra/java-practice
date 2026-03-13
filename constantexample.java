e1208a27ec153c37a6bd836a88fc452484254add
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 18:45:52 +0530

    corrected the case-sensetive error in line 4 , changed FINAL to final

---
+++ constantexample.java
@@ -0,0 +1,9 @@
+class constantexample{
+    public static void main(String[]args){
+        final double PI=3.14159;
+        final int max_users=100;
+
+        System.out.println("pi:"+PI);
+        System.out.println("max users:"+max_users);
+    }
+}
