e772c051e68c68122a4da51b38e2d31b1ec6d23e
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 18:39:43 +0530

    Add VariableExample class to demonstrate variable declaration and initialization in Java

---
+++ VariableExample.java
@@ -0,0 +1,13 @@
+class VariableExample{
+    public static void main(String[]args){
+        int age=25;
+        double salary=45000.5;
+        char grade='A';
+        boolean passed=true;
+
+        System.out.println("age:"+age);
+        System.out.println("salary:"+salary);
+        System.out.println("grade:"+grade);
+        System.out.println("passed:"+passed);
+    }
+}
