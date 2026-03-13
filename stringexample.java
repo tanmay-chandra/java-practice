ccd60913d571d7699bea9e4f0457b0ea692eba39
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 19:01:48 +0530

    Add stringexample class to demonstrate string manipulation in Java

---
+++ stringexample.java
@@ -0,0 +1,11 @@
+class stringexample{
+    public static void main(String[]args){
+        String name="Shauryansh";
+
+        System.out.println("Length:"+name.length());
+
+        System.out.println("uppercase:"+name.toUpperCase());
+
+        System.out.println("Character at index 2:"+name.charAt(2));
+    }
+}
