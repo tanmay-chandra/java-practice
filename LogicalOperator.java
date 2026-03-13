5b766f86df7b0d2792cd97509abcf7fd9facd236
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 15:46:49 +0530

    Add LogicalOperator class to demonstrate logical operators in Java

---
+++ LogicalOperator.java
@@ -0,0 +1,10 @@
+public class LogicalOperator{
+    public static void main(String[]args){
+        boolean x=true,y=false;
+
+        System.out.println("x&&y;"+(x&&y));
+        System.out.println("x||y:"+(x||y));
+        System.out.println("!x:"+(!x));
+        
+    }
+}
