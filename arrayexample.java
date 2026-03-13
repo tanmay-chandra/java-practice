40a8cf2fb6300e69f11c3912fb8fcc5a2e38d6d0
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 18:53:49 +0530

    Add arrayexample class to demonstrate array initialization and iteration in Java

---
+++ arrayexample.java
@@ -0,0 +1,10 @@
+class arrayexample{
+    public static void main(String[]args){
+
+        int[]numbers={10,20,30,40,50};
+        for(int i=0;i<numbers.length;i++){
+
+            System.out.println("element:"+i+":"+numbers[i]);
+        }
+    }
+}
