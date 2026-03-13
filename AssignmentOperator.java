245713b713e85cb61b2cc5684d461dda4c6f7ac9
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-13 15:59:58 +0530

    Add AssignmentOperator class to demonstrate assignment operators in Java

---
+++ AssignmentOperator.java
@@ -0,0 +1,17 @@
+public class AssignmentOperator{
+    public static void main(String[]args){
+        
+        int a=10;
+        a+=5;
+        System.out.println("a+=5:"+a);
+        a-=3;
+        System.out.println("a-=3:"+a);
+        a*=2;
+        System.out.println("a*2:"+a);
+        a/=4;
+        System.out.println("a/4:"+a);
+        a%=3;
+        System.out.println("a%3:"+a);
+        
+    }
+}
