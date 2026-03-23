e831bead9608b54c2bf5d6a5c3929437773393df
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-23 21:34:55 +0530

    add : instead of ; infront of outerLoop

--- labeledbreakexample.java
+++ labeledbreakexample.java
@@ -1,16 +1,17 @@
 public class labeledbreakexample{
     public static void main(String[]args){
 
-        outerLoop;
+        outerLoop:
         for(int i=1;i<=3;i++){
             for(int j=1;j<=3;j++){
 
-                if(i==2&&j==2){
+                if(i==2 && j==2){
                     break outerLoop;
                 }
-            }
             
-            System.out.println(i+""+j);
+            
+            System.out.println(i +" "+ j);
+            }
         }
     }
 }
