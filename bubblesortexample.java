47a7b4f0a80786e06d1bc70c0381278729fe1a0e
tanmay-chandra <tanmaychandra912@gmail.com>
2026-03-24 23:00:22 +0530

    sorting technique-1

--- bubblesortexample.java
+++ bubblesortexample.java
@@ -4,14 +4,17 @@ public class bubblesortexample{
     public static void bubblesort(int[]arr){
         int n=arr.length;
         boolean swapped;
-        for(int i=0;i<n-1;i++){
-            swapped=false;
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
+        for(int i=0;i<n-1;i++){         //outer loop to check rounds
+            swapped=false;              //assume no swapping happened
+        for(int j=0;j<n-i-1;j++){       //inner loop,compares nearby elements
+
+            if(arr[j]>arr[j+1]){        //swapping as ascending to descending
+
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
                 swapped=true;
+
             }
         }
         
@@ -19,9 +22,11 @@ public class bubblesortexample{
     }
 }
 public static void main(String[]args){
+
     int[]arr={5,3,1};
     bubblesort(arr);
     for(int num:arr)
         System.out.println(num+"");
+
 }
 }
