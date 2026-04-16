445f55f553a36db82d643846c690d48ed0b22db4
tanmay-chandra <tanmaychandra912@gmail.com>
2026-04-16 11:06:01 +0530

    error fixed

---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+class Car{
+    String brand="toyota";
+    int speed=120;
+}
+public class Vehicle{
+    public static void main(String[]args){
+        Car c1=new Car();
+        System.out.println("brand:"+c1.brand);
+        System.out.println("speed:"+c1.speed);
+
+    }
+}
--- main.java
+++ main.java
@@ -1,18 +1,18 @@
-class student{
+class Student {
     int id;
     String name;
-    void display(){
-        System.out.println("Student ID:"+id);
-        System.out.println("Student Name:"+name);
+
+    void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
     }
 }
 
-public class main{
-    public static void main(String[]args){
-
-        student s=new student();
-        s.id=101;
-        s.name="tanmay";
-        s.display();
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.id = 101;
+        student.name = "tanmay";
+        student.display();
     }
 }
