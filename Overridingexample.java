292aa0e82e056c925b98340835f1896e13841319
tanmay-chandra <tanmaychandra912@gmail.com>
2026-04-20 21:02:57 +0530

    add Overridingexample class demonstrating method overriding in Java

---
+++ Overridingexample.java
@@ -0,0 +1,17 @@
+class Animal{
+    void sound(){
+        System.out.println("Animal makes sound");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+
+public class Overridingexample{
+    public static void main(String[]args){
+        Dog d=new Dog();
+        d.sound();
+    }
+}
