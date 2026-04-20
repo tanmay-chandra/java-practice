cd0af13927e14c972cc846895e061e872bbc287b
tanmay-chandra <tanmaychandra912@gmail.com>
2026-04-20 22:54:31 +0530

    .

---
+++ InheritanceDemo.java
@@ -0,0 +1,18 @@
+class Vehicle{
+    void start(){
+        System.out.println("Vehicle start");
+    }
+}
+class Car extends Vehicle{
+    void drive(){
+        System.out.println("car drives");
+    }
+}
+
+public class InheritanceDemo{
+    public static void main(String[]args){
+        Car c=new Car();
+        c.start();
+        c.drive();
+    }
+}
